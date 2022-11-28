06974c125fe6b74c659b75ca05e07783a9770449
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2022-11-28 12:16:08 +0100

    router and context setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import {Routes, Route} from "react-router-dom"
+import SeilessProvider from "./context/SeilessProvider"
+import Homepage from "./pages/Homepage"
+import SharedHeader from "./components/header/SharedHeader"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import SinglePage from "./pages/SinglePage"
+import Error from "./pages/Error"
 
 
 function App() {
   return (
-    <div>
-      Home - New
-    </div>
+    <SeilessProvider>
+    <Routes>
+      <Route path="/" element={<SharedHeader/>}>
+      <Route index element={<Homepage />}/>
+      <Route path="about" element={<About />}/>
+      <Route path="contact" element={<Contact />}/>
+      <Route path="photos/:ID" element={<SinglePage />}/>
+      <Route path="*" element={<Error />}/>
+      </Route>
+    </Routes>
+    </SeilessProvider>
   );
 }
 
---
+++ src/context/SeilessProvider.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SeilessContext from "./SeilessContext"
+import { useState } from 'react'
+
+const SeilessProvider = (props) => {
+
+const [photos, setPhotos] = useState([])
+
+  return (
+    <SeilessContext.Provider value={{photos}}>
+        {props.children}
+    </SeilessContext.Provider>
+  )
+}
+
+export default SeilessProvider
